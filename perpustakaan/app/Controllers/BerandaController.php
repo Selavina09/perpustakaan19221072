ecaea8317235094a6965ac68eac4e11672c39551
selavina19221072 <tikadwikatika@gmail.com>
2023-09-25 14:15:33 +0700

    Penambahan controller login

---
+++ perpustakaan/app/Controllers/BerandaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class BerandaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function judul()
+    {
+        //
+    }
+
+    public function gambar()
+    {
+        //
+    }
+    public function konten_terkini()
+    {
+       // 
+    }
+    
+}
